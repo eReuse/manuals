1fa4c8861ac04e9e68a6c3e6e152cc28e0484fba
Cayo Puigdefabregas <cayo@usody.com>
2023-07-05 11:55:42 +0200

    add basic icecat api

--- views.py
+++ views.py
@@ -7,14 +7,15 @@ from models import Category, Guide, Icecat
 logger = logging.getLogger(__name__)
 
 
-ifixit = Blueprint('ifixit', __name__, url_prefix='/ifixit')
+ifixit = Blueprint('ifixit', __name__, url_prefix='/')
 
 
-class HomeView(View):
+class ManualsMix(View):
     methods = ['POST']
 
     def dispatch_request(self):
         try:
+            self.get_params()
             res = self.get_values()
         except Exception as err:
             logger.error("Error: {}".format(err))
@@ -27,31 +28,62 @@ class HomeView(View):
             res.append(q.get_json())
         return res
 
-    def query(self):
+    def get_params(self):
         req = request.get_json()
-        manufacturer = req.get('manufacturer')
-        model = req.get('model')
-        q = req.get('query')
-        if not any([manufacturer, model, q]):
+        self.manufacturer = req.get('manufacturer')
+        self.model = req.get('model')
+        self.q = req.get('query')
+        if not any([self.manufacturer, self.model, self.q]):
             raise Exception('No there are any query')
 
+    def query(self):
+        """
+            Need to implement
+        """
+        return []
+
+
+class IfixitView(ManualsMix):
+    def query(self):
         query = Guide.query
 
-        if manufacturer:
+        if self.manufacturer:
             query = query.filter(
-                Guide.title.icontains(manufacturer)
+                Guide.title.icontains(self.manufacturer)
             )
 
-        if model:
+        if self.model:
             query = query.filter(
-                Guide.title.icontains(model)
+                Guide.title.icontains(self.model)
             )
 
-        if q:
+        if self.q:
             query = query.filter(
-                Guide.title.icontains(q)
+                Guide.title.icontains(self.q)
             )
         return query
 
 
-ifixit.add_url_rule('/', view_func=HomeView.as_view('home'))
+class IcecatView(ManualsMix):
+    def query(self):
+        query = Guide.query
+
+        if self.manufacturer:
+            query = query.filter(
+                Guide.title.icontains(self.manufacturer)
+            )
+
+        if self.model:
+            query = query.filter(
+                Guide.title.icontains(self.model)
+            )
+
+        if self.q:
+            query = query.filter(
+                Guide.title.icontains(self.q)
+            )
+        return query
+
+
+ifixit.add_url_rule('/ifixit', view_func=IfixitView.as_view('ifixit'))
+ifixit.add_url_rule('/icecat', view_func=IcecatView.as_view('icecat'))
